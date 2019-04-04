f41c0aca8655cc9a877e134ebfefb4f1a09bd2d2
ebrault <emmet.brault@gmail.com>
2019-04-04 16:06:33 -0400

    Refactored App Component
    Refactored App component to show how JSX works with JS variables
      - Initialized variable buttonText and assigned it to string Click me
      - Replaced text field for button element ('Submit') with {buttonText}
        - JS variables can be passed into JSX elements using the {} syntax
      - Successfully observed change rendered in app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const buttonText = 'Click me!'
   return (
     <div>
       <label className="label" for="name">Enter name: </label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText}
+      </button>
     </div>
   );
 }
