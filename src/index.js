ef2936ce822c88dc9fda9012e8337775119d8025
ebrault <emmet.brault@gmail.com>
2019-04-04 16:23:42 -0400

    Final Refactor To App Component
    Final refactor to app component for exercise.
      - Exercise asks to change the label text to be its own variable
        - Remove 'Enter name: ' from the JSX element and assign it to
          variable labelName
        - Pass variable to JSX element
      - Successful, no observable changes in browser

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const buttonText = {text: 'Click me!'}
+  const labelText = 'Enter name: '
   return (
     <div>
-      <label className="label" htmlFor="name">Enter name: </label>
+      <label className="label" htmlFor="name">{labelText}</label>
       <input id="name" type="text" />
       <button style={{backgroundColor: 'blue', color: 'white'}}>
         {buttonText.text}
