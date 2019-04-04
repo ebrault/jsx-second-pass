83eaff25b3fae6d30fcdf577e007deb640702f5c
ebrault <emmet.brault@gmail.com>
2019-04-04 15:56:57 -0400

    Refactored App Component
    Refactored App component to render new elements to the DOM
      - Created multi-line JSX by wrapping the return div in parens
      - Added label, input for, and button into return div with selectors
        from CodePen.
      - Slightly refactored code from CodePen
        - In order to demo similarities/diffs between HTML/JSX, copied and
          pasted HTML into div. JSX is not HTML, so refactored to
          JSX conventions.
            - "className" instead of "class" because JavaScript's class
              keyword exists in JSX
            - Inline styling using objects with key/value pairs and camel
              case

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <label className="label" for="name">Enter name: </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
